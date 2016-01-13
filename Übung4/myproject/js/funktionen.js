7e9d65ff3e05011ecd9a97ed67d110312b540de6
T0biWan <Tobi.Klatt@web.de>
2016-01-13 20:16:20 +0100

    Sicherheitscommit

--- Übung4/myproject/js/funktionen.js
+++
@@ -1,8 +0,0 @@
-$(window).load(function() {
-	$("#bearbeiten").click(function(){
-		// location.reload();
-		$document.location.href='http://localhost/mme/bearbeiten.php';
-		
-	});
-	console.log("initialisiert")
-});
